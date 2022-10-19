28fe606dcde0ce790ed39a83e8ec25b6c055e59f
Francis Ogertschnig <Francis.Ogertschnig@gmail.com>
2022-10-19 15:44:56 -0700

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (wordToReverse) {
+    let newLength = wordToReverse.length - 1;
+    let newWord = '';
 
+    while (newLength >= 0) {
+        newWord = newWord + wordToReverse[newLength];
+        newLength--;
+    }
+
+    return newWord;
 };
 
 // Do not edit below this line
