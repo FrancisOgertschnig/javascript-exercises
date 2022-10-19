8d55aa637c1ee5236ad5536b48302d3539943031
Francis Ogertschnig <Francis.Ogertschnig@gmail.com>
2022-10-19 15:01:01 -0700

    update github

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let i = 1;
+    let sentence = '';
+    while (i <= num) {
+        sentence = sentence + word;
+        i++;
+    }
+
+    return sentence;
 };
 
 // Do not edit below this line
